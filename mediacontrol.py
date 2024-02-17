7d3bc380348372dfd14560c8913c16f04e7e6c7d
denz8028 <125301239+denz8028@users.noreply.github.com>
2024-02-17 19:40:19 +0300

    Update mediacontrol.py

--- mediacontrol.py
+++ mediacontrol.py
@@ -1,15 +1,21 @@
 import keyboard
 import os
+import subprocess
+import time
+def track():
+	result = subprocess.run(['/bin/bash', 'dumpsys.sh'], stdout=subprocess.PIPE)
+	print(result.stdout.decode('utf-8')) # print trackname output
 def nextmedia():
     os.system("adb shell input keyevent 87")
+    track()
 def prevmedia():
 	os.system("adb shell input keyevent 88")
+	track()
 def playpause():
 	os.system("adb shell input keyevent 85")
-
+	track()
 keyboard.add_hotkey('ctrl+shift+g', nextmedia)
 keyboard.add_hotkey('ctrl+shift+f', prevmedia)
 keyboard.add_hotkey('ctrl+shift+d', playpause)
 keyboard.wait('Ctrl + Q')
-
 
